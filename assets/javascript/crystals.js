f875024fcb55ad169c6fb0625276f345dc57d533
Nick West <mail@nickwe.st>
2016-05-27 19:48:44 -0400

    Crystal game almost finished

--- assets/javascript/crystals.js
+++ assets/javascript/crystals.js
@@ -1,9 +1,12 @@
 window.onload = function(){
 	$('#ruby').click(crystals.rubyTouch);
 	$('#geode').click(crystals.geodeTouch);
+	$('#sapphire').click(crystals.sapphireTouch);
+	$('#amulet').click(crystals.amuletTouch);
 };
-	
+
 	var pwr = 0;
+	var cash = 0;
 	var crystals = {
 			rubyTouch: function(){
 				pwr += 33;
@@ -12,5 +15,18 @@ window.onload = function(){
 			geodeTouch: function(){
 				pwr += 3;
 				$("#pwrCounter").html(pwr);
+			},
+			sapphireTouch: function(){
+				pwr += 200;
+				$("#pwrCounter").html(pwr);
+			},
+			amuletTouch: function(){
+				if (pwr >= 999 && pwr <= 9000){
+					$("#container");
+				}
+				else{
+				}
 			}
 	};
+
+		
